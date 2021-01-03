80e858e3d9494e1cc449b09334de0bfedeabce8c
Rakesh Kirola <55341835+urakesh661@users.noreply.github.com>
2021-01-03 19:09:38 +0530

    Update rkrecon.sh

--- rkrecon.sh
+++ rkrecon.sh
@@ -9,11 +9,11 @@ word=$2
 #path=$3
 dt=$(date +%F.%H.%M.%S)
 
-toolsDir=~/tools
+toolsDir=/media/rocky2311/210cf76e-f990-4ec3-82fc-cdef7cc951691/Recon_Auto/tools
 resultDirWebSS=$toolsDir/results/$domain-$dt/WebScreenshot
 resultDir=$toolsDir/results/$domain-$dt
 resultDirNMap=$toolsDir/results/$domain-$dt/NMap
-basedir=~/tools/results
+basedir=/media/rocky2311/210cf76e-f990-4ec3-82fc-cdef7cc951691/Recon_Auto/tools/results
 jsfilesDir=$toolsDir/results/$domain-$dt/JSFiles
 
 mkdir -p $resultDir
@@ -67,11 +67,19 @@ recon_assetfinder(){
     echo -e "${BOLD}${LIGHT_GREEN}Subdomain scanning completed using asset finder!${NORMAL}"
 }
 
-find_subdomains(){
 
+recon_webarchive(){
+	 echo -e "${BOLD}${LIGHT_GREEN}Subdomain scanning started using web archive!${NORMAL}"
+	 webarchiveOutput=$resultDir/webarchive_$domain.txt
+	 curl -s "http://web.archive.org/cdx/search/cdx?url=*.$domain/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' |sort -u >> $webarchiveOutput
+	
+}
+
+
+find_subdomains(){
     recon_findomain
     recon_assetfinder
- 
+    recon_webarchive
  }
 
 find_subdomains
@@ -88,10 +96,13 @@ do
                
         mv $domain.txt $findomainOutput
         
-        sort -u $findomainOutput $assetfinderOutput  > $resultDir/$domain.subdomains.txt
+        sort -u $findomainOutput $assetfinderOutput $webarchiveOutput >> $resultDir/$domain.subdomains.txt
 		sleep 2
-		grep "$domain" $resultDir/$domain.subdomains.txt > $resultDir/$domain.valsubdomains.txt  
-		awk !/$word/  $resultDir/$domain.valsubdomains.txt > $resultDir/$domain.validsubdomains.txt
+		grep "$domain" $resultDir/$domain.subdomains.txt >> $resultDir/$domain.valsubdomains.txt  
+		awk !/$word/  $resultDir/$domain.valsubdomains.txt >> $resultDir/$domain.validsubdomain.txt
+		sed -i "/\b\(-\|,\)\b/d" $resultDir/$domain.validsubdomain.txt
+		sed '/[0-9]/d' $resultDir/$domain.validsubdomain.txt >> $resultDir/$domain.validsubdomainss.txt
+		awk '!/\-/' $resultDir/$domain.validsubdomainss.txt  >> $resultDir/$domain.valsubdomainss.txt
 		
         echo -en "\rTime elapsed : $totalTime seconds"
         break;
@@ -102,12 +113,12 @@ do
 done
 echo ""
 echo -e "${BOLD}${LIGHT_GREEN}Done finding subdomains${NORMAL}"
-echo -e "${BOLD}${LIGHT_GREEN}Total subdomains found : `wc -l $resultDir/$domain.validsubdomains.txt`${NORMAL}"
+echo -e "${BOLD}${LIGHT_GREEN}Total subdomains found : `wc -l $resultDir/$domain.valsubdomainss.txt`${NORMAL}"
 
 recon_resdomains(){
 
 		echo -e "${BOLD}${LIGHT_GREEN}Fetching of resolved subdomains using httprobe started!${NORMAL}"
-		cat $resultDir/$domain.validsubdomains.txt | httprobe -c 50 > $resultDir/httprobe.$domain.txt  
+		cat $resultDir/$domain.valsubdomainss.txt | httprobe -c 40 >> $resultDir/httprobe.$domain.txt  
 		echo -e "${BOLD}${LIGHT_GREEN}Fetching of resolved subdomains using httprobe completed!${NORMAL}"
 }
 
@@ -115,7 +126,7 @@ recon_resdomains(){
 recon_waybackurls(){
 
 		echo -e "${BOLD}${LIGHT_GREEN}Fetching of url's from way back machine started!${NORMAL}"
-		cat $resultDir/$domain.validsubdomains.txt | waybackurls > $resultDir/waybackurl_$domain.txt
+		cat $resultDir/$domain.valsubdomainss.txt | waybackurls >> $resultDir/waybackurl_$domain.txt
 		echo -e "${BOLD}${LIGHT_GREEN}Fetching of url's from way back machine completed!${NORMAL}"
 }
 
@@ -123,7 +134,7 @@ recon_waybackurls(){
 recon_naabu(){
 
 	echo -e "${BOLD}${LIGHT_GREEN}Port scanning using Naabu started!${NORMAL}"
-	naabu -silent -iL $resultDir/$domain.validsubdomains.txt -o $resultDirNMap/naabu_portscanning_result.txt > /dev/null
+	naabu -silent -iL $resultDir/$domain.valsubdomainss.txt -o $resultDirNMap/naabu_portscanning_result.txt
         echo -e "${BOLD}${LIGHT_GREEN}Port scanning using Naabu completed!${NORMAL}"
 }
 
@@ -131,7 +142,8 @@ recon_naabu(){
 recon_jslinks(){
 	
 	echo -e "${BOLD}${LIGHT_GREEN}Fetching of jslinks started!${NORMAL}"
-	xargs -P 500 -a  $resultDir/$domain.validsubdomains.txt -I@ sh -c 'nc -w1 -z -v @ 443 2>/dev/null && echo @' | xargs -I@ -P10 sh -c 'gospider -a -s "https://@" -d 2 | grep -Eo "(http|https)://[^/\"].*\.js+" | sed "s#\] \-  #\n#g" | anew' > $resultDir/subdomain_js_files.txt
+	cut -d'/' -f3 $resultDir/httprobe.$domain.txt >> $resultDir/httprobefinal.$domain.txt
+	xargs -P 500 -a  $resultDir/httprobefinal.$domain.txt -I@ sh -c 'nc -w1 -z -v @ 443 2>/dev/null && echo @' | xargs -I@ -P10 sh -c 'gospider -a -s "https://@" -d 2 | grep -Eo "(http|https)://[^/\"].*\.js+" | sed "s#\] \-  #\n#g" | anew' >> $resultDir/subdomain_js_files.txt
 	echo -e "${BOLD}${LIGHT_GREEN}Fetching of jslinks completed!${NORMAL}"
 	
 	echo -e "${BOLD}${LIGHT_GREEN}Fetching of endpoints,parameters etc. started!${NORMAL}"
@@ -143,8 +155,9 @@ recon_jslinks(){
 	do
 		python3 $toolsDir/LinkFinder/linkfinder.py -i $file -o cli >> /$resultDir/endpoints.txt
 	done
-	grep -Fvxf /home/rocky2311/nottobe_included.txt /$resultDir/endpoints.txt > /$resultDir/endpoints_final.txt 
-	echo -e "${BOLD}${LIGHT_GREEN}Fetching of endpooints,parameters etc. completed!${NORMAL}"
+	grep -Fvxf /media/rocky2311/210cf76e-f990-4ec3-82fc-cdef7cc951691/Recon_Auto/nottobe_included.txt /$resultDir/endpoints.txt >> /$resultDir/endpoints_final.txt 
+	sort /$resultDir/endpoints_final.txt | uniq -d  >> /$resultDir/js_endpoints.txt  
+	echo -e "${BOLD}${LIGHT_GREEN}Fetching of endpoints,parameters etc. completed!${NORMAL}"
 	
 }
 
@@ -152,16 +165,16 @@ recon_jslinks(){
 recon_wed_dir_file_fuzzing(){
 	
 	echo -e "${BOLD}${LIGHT_GREEN}File & directory discovery process started!${NORMAL}"
-	for end in $(cat $resultDir/httprobe.$domain.txt); do ffuf -w /home/rocky2311/generic_crowd_sourced_new.txt -u $end/FUZZ -mc 200,202,203,403 -t 2 -r -recursion 2 -s ;done > $resultDir/subdomaindata_httprobe_fuzzing.txt 
+	for end in $(cat $resultDir/httprobe.$domain.txt); do ffuf -w /media/rocky2311/210cf76e-f990-4ec3-82fc-cdef7cc951691/Recon_Auto/generic_crowd_sourced_new.txt -u $end/FUZZ -mc 200,202,203 -t 4 -r -recursion 2 -s ;done >> $resultDir/subdomaindata_httprobe_fuzzing.txt 
 	sed $'s/[^[:print:]\t]//g' $resultDir/subdomaindata_httprobe_fuzzing.txt
-	sed -r 's/.{3}//' $resultDir/subdomaindata_httprobe_fuzzing.txt > $resultDir/subdomaindata_httprobe_fuzzing_final.txt
+	sed -r 's/.{5}//' $resultDir/subdomain_httprobe_fuzzing.txt >> $resultDir/subdomain_fuzzing.txt
 	echo -e "${BOLD}${LIGHT_GREEN}File & directory discovery process completed!${NORMAL}"
 }
 
 recon_screenshot(){
 
 	echo -e "${BOLD}${LIGHT_GREEN}Screen shot process started for domains resolved through httprobe!${NORMAL}"
-	awk !/'.js'/ $resultDir/waybackurl_$domain.txt > $resultDir/waybackurl_nojs.$domain.txt 
+	#awk !/'.js'/ $resultDir/waybackurl_$domain.txt >> $resultDir/waybackurl_nojs.$domain.txt 
 	python $toolsDir/webscreenshot/webscreenshot.py -i $resultDir/httprobe.$domain.txt -o $resultDirWebSS -q 05 -t 60
 	echo -e "${BOLD}${LIGHT_GREEN}Screen shot process completed for domains resolved through httprobe!${NORMAL}"
 	
@@ -170,56 +183,53 @@ recon_screenshot(){
 recon_screenshot_waybackurl(){	
 	echo -e "${BOLD}${LIGHT_GREEN}Screen shot process started for url's discovered using waybackmachine!${NORMAL}"
 	
-	cat $resultDir/waybackurl_nojs.$domain.txt | medic -c 30 > $resultDir/url_responsecode.txt
-	awk '$3 == 200' $resultDir/url_responsecode.txt > $resultDir/url_code_200.txt
-	awk '$3 == 403' $resultDir/url_responsecode.txt > $resultDir/url_code_403.txt
+	cat $resultDir/waybackurl_nojs.$domain.txt | medic -c 30 >> $resultDir/url_responsecode.txt
+	awk '$3 == 200' $resultDir/url_responsecode.txt >> $resultDir/url_code_200.txt
+	awk '$3 == 403' $resultDir/url_responsecode.txt >> $resultDir/url_code_403.txt
 	
-	awk '{ print $4 }' $resultDir/url_code_200.txt > $resultDir/url_responsecode_200.txt
-	awk '{ print $4 }' $resultDir/url_code_403.txt > $resultDir/url_responsecode_403.txt
+	awk '{ print $4 }' $resultDir/url_code_200.txt >> $resultDir/url_responsecode_200.txt
+	awk '{ print $4 }' $resultDir/url_code_403.txt >> $resultDir/url_responsecode_403.txt
 	
-	python $toolsDir/webscreenshot/webscreenshot.py	-i $resultDir/url_responsecode_200.txt -o $resultDirWebSS -q 05 -t 60
-        python $toolsDir/webscreenshot/webscreenshot.py	-i $resultDir/url_responsecode_403.txt -o $resultDirWebSS -q 05 -t 60
+	python $toolsDir/webscreenshot/webscreenshot.py -i $resultDir/url_responsecode_200.txt -o $resultDirWebSS -q 05 -t 60
+        python $toolsDir/webscreenshot/webscreenshot.py -i $resultDir/url_responsecode_403.txt -o $resultDirWebSS -q 05 -t 60
 	echo -e "${BOLD}${LIGHT_GREEN}Screen shot process completed for url's discovered using waybackmachine!${NORMAL}"        
 }
 
 
 recon_domain_diff(){
 
-rm -rf $resultDir/$domain.valsubdomains.txt $resultDir/$domain.subdomains.txt  $resultDir/findomain_$domain.txt $resultDir/assetfinder_$domain.txt $resultDir/subdomaindata_httprobe_fuzzing.txt $resultDir/endpoints.txt
+#rm -rf $resultDir/$domain.valsubdomains.txt $resultDir/$domain.subdomains.txt  $resultDir/findomain_$domain.txt $resultDir/assetfinder_$domain.txt $resultDir/subdomaindata_httprobe_fuzzing.txt $resultDir/endpoints.txt $resultDir/endpoints_final.txt $resultDir/$domain.valsubdomains.txt $resultDir/$domain.validsubdomain.txt $resultDir/$domain.validsubdomainss.txt $resultDir/webarchive_$domain.txt
 
 ENDTIME=$(date +%s)
 totalTime=$(( $ENDTIME-$STARTTIME ))
 
 
 domain_diff=$domain
-domain_regexp=*
+domain_regexp="*"
 
 echo "Subdomain diff. comparison process started---->" $domain
-
 sleep 2
+find $basedir/ -maxdepth 1 -name  ${domain_diff}"${domain_regexp}" -type d -exec readlink -f {} \; > $resultDir/names.txt
 
-find $basedir/ -maxdepth 1 -name  ${domain_diff}${domain_regexp} -type d -exec readlink -f {} \; > $resultDir/names.txt
+#echo ${domain_diff}"${domain_regexp}"
+#echo -n ${domain_diff};echo "$domain_regexp" | xargs
+
+#{ echo ${domain_diff} ; echo "$domain_regexp";}
 
 if (($(wc -l < $resultDir/names.txt) <= 1 ));then exit 1
 fi
-sort -k1 -r $resultDir/names.txt > $resultDir/sorted.txt
-
-head -2 $resultDir/sorted.txt > $resultDir/names_lim.txt
-
-awk '{ print $0}' $resultDir/names_lim.txt | awk -F'/' '{print $6}' > $resultDir/na_lim_split.txt
-
+sort -k1 -r $resultDir/names.txt >> $resultDir/sorted.txt
+head -2 $resultDir/sorted.txt >> $resultDir/names_lim.txt
+awk '{ print $1}' $resultDir/names_lim.txt | awk -F'/' '{print $8}' >> $resultDir/na_lim_split.txt
 line1=$(head  -1 $resultDir/na_lim_split.txt)
 line2=$(head  -2 $resultDir/na_lim_split.txt | tail -1)
 
-
-diff -bir $basedir/$line1/$domain.validsubdomains.txt $basedir/$line2/$domain.validsubdomains.txt | sort > $resultDir/${domain_diff}_subdomains_new.txt
-
+diff -bir $basedir/$line1/$domain.valsubdomainss.txt $basedir/$line2/$domain.valsubdomainss.txt | sort >> $resultDir/${domain_diff}_subdomains_new.txt
 #cat $basedir/subdomains_new.txt
 
 rm -rf $resultDir/na_lim_split.txt $resultDir/names.txt $resultDir/names_lim.txt $resultDir/sorted.txt
 
 curl  --silent --output /dev/null -F "chat_id=731636917" -F document=@/$resultDir/${domain_diff}_subdomains_new.txt https://api.telegram.org/bot1345450515:AAFQMWbmxpMT1OznO7mN9IlIW8Xy5-CR12M/sendDocument 
-
 
 echo -en "\rTime elapsed : ${BLINK}${LIGHT_GREEN}$totalTime${NORMAL} seconds"
 echo -e "Results in : ${LIGHT_GREEN}$resultDir${NORMAL}"
